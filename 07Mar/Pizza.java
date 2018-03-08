efdb97e0cbb7930208e120b6dca05fd73c70dd23
isela9727 <icanale@cetys.edu.mx>
2018-03-08 12:05:12 -0800

    correcciones
    pizza

--- 07Mar/Pizza.java
+++ 07Mar/Pizza.java
@@ -3,8 +3,22 @@ public abstract class Pizza implements Procedimientos
 {
 	String pizza;
 	
-	public Pizza orderPizza(String pizza)
+	public Pizza orderPizza(Pizza pizza)
 	{
-		Pizza 
+		pizza.preparar();
+		pizza.hornear();
+		pizza.cortar();
+		pizza.empacar();
+		pizza.enviar();
+		return pizza;
+	}
+	
+	public void pizzaMaker(Pizza pizza)
+	{
+		pizza.preparar();
+		pizza.hornear();
+		pizza.cortar();
+		pizza.empacar();
+		pizza.enviar();
 	}
 }
--- 07Mar/PizzaApp.java
+++ 07Mar/PizzaApp.java
@@ -2,28 +2,13 @@
 public class PizzaApp 
 {
 	public static void main (String []args)
-	{
+	{		
 		Pizza vegetariana = new Vegetariana();
 		Pizza vegana = new Vegana();
 		Pizza mexicana = new Mexicana();
 		
-		vegetariana.preparar();
-		vegetariana.hornear();
-		vegetariana.cortar();
-		vegetariana.empacar();
-		vegetariana.enviar();
-		
-		vegana.preparar();
-		vegana.hornear();
-		vegana.cortar();
-		vegana.empacar();
-		vegana.enviar();
-		
-		mexicana.preparar();
-		mexicana.hornear();
-		mexicana.cortar();
-		mexicana.empacar();
-		mexicana.enviar();
-		//vegetariana = PizzaMaker("pizza");
+		vegetariana.orderPizza(vegetariana);
+		vegana.orderPizza(vegana);
+		mexicana.orderPizza(mexicana);		
 	}	
 }

2478d068e332838a27eb79bdf0c8cc1c45b3b588
isela9727 <icanale@cetys.edu.mx>
2018-04-26 15:07:26 -0700

    18abr
    operaciones added

--- 18AbrAct1/HelloWorldView.java
+++ 18AbrAct1/HelloWorldView.java
@@ -4,6 +4,12 @@ public class HelloWorldView
 	public static void main (String args[])
 	{
 		HelloWorldController helloController = new HelloWorldController();
-		System.out.print(helloController.helloWorld());
+		System.out.println(helloController.helloWorld());
+		
+		OperacionesController operacion = new OperacionesController();
+		System.out.println("El resultado es: " + operacion.suma(5, 6));
+		System.out.println("El resultado es: " + operacion.resta(8, 3));
+		System.out.println("El resultado es: " + operacion.multiplicacion(6,  8));
+		System.out.println("El resultado es: " + operacion.division(9, 3));
 	}	
 }
---
+++ 18AbrAct1/OperacionesController.java
@@ -0,0 +1,23 @@
+
+public class OperacionesController 
+{
+	public int suma(int x, int y)
+	{
+		return x + y;
+	}
+	
+	public int resta(int x, int y)
+	{
+		return x - y;
+	}
+	
+	public int multiplicacion(int x, int y)
+	{
+		return x * y;
+	}
+	
+	public int division(int x, int y)
+	{
+		return x / y;
+	}
+}
